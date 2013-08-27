15eaaa3e27455378a55dc394ff0d88b459ac6a69
Matt Briggs <matt@mattbriggs.net>
2013-08-27 13:37:47 -0400

    adds semis, and adds better backwards compat.

--- simple-require.js
+++ simple-require.js
@@ -2,61 +2,60 @@
   "use strict";
 
   var cache = {},
-      definitions = {}
+      definitions = {};
 
   var require = function(name){
-    var module
+    var module;
 
     if(cache[name]){
       module = cache[name]
     } else if(definitions[name] === undefined){
-      throw new Error("Module '"+ name +"' must be defined!")
+      throw new Error("Module '"+ name +"' must be defined!");
     } else {
-      module = cache[name] = resolve( name, definitions[name] )
+      module = cache[name] = resolve( name, definitions[name] );
     }
 
-    return module
-  }
+    return module;
+  };
 
   var define = function(name, definition){
-    if(definitions[name]) throw new Error(name +" has already been defined!")
+    if(definitions[name]) throw new Error(name +" has already been defined!");
 
-    definitions[name] = definition
-  }
+    definitions[name] = definition;
+  };
 
   function resolve(name, definition){
-    var module = { exports: {} }
+    var module = { exports: {} };
 
-    definition(module, module.exports)
+    definition(module, module.exports);
 
     if(canLog() && !hasProps(module.exports)){
-      console.error(name + " doesn't export anything!!!! Maybe you want to look into that?")
+      console.error(name + " doesn't export anything!!!! Maybe you want to look into that?");
     }
 
-    return module.exports
+    return module.exports;
   }
 
   function canLog(){
-    return window.console && console.error
+    return window.console && console.error;
   }
 
   function hasProps(obj){
-    if(!Object.keys) return
+    if(typeof obj !== 'object') return true;
+    var props = [];
 
-    if(typeof obj === 'object'){
-      return Object.keys(obj).length > 0
-    } else {
-      return true
-    }
+    for(var prop in obj){ props.push(prop); }
+
+    return props.length > 0;
   }
 
   define._clearCaches = function(){
-    cache = {}
-  }
+    cache = {};
+  };
 
-  if(window.define) throw new Error("Simple Require: define already exists!")
-  if(window.require) throw new Error("Simple Require: require already exists!")
+  if(window.define) throw new Error("Simple Require: define already exists!");
+  if(window.require) throw new Error("Simple Require: require already exists!");
 
-  window.define = define
-  window.require = require
+  window.define = define;
+  window.require = require;
 }());
